c1fb995aef363fec702adf1f89f5c0f0b073f25e
Harsimranjit Kaur <harsimranchahal170@gmail.com>
2020-08-04 11:55:15 -0700

    adding migration for participants and start_time

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,13 +1,23 @@
 class Event < ApplicationRecord
     before_validation :set_default_price
+    before_validation :set_default_participants
 
     validates :title, presence: true, uniqueness: true
     validates :description, presence: true, length: { minimum: 100 }
     validates :location, presence: true
     validates :price, presence: true
     validates :image, presence: true
+    validates :participants, presence: true, numericality: {less_than: 21}
+    validates :start_time, presence: true
+    
     private
+
     def set_default_price
         self.price ||=0
     end
+
+    def set_default_participants
+        self.participants ||=0
+    end
+
 end
