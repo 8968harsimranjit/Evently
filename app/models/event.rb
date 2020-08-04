68e7ce93e6d2933f2eb1ac6986c5a6ca9eee0298
Harsimranjit Kaur <harsimranchahal170@gmail.com>
2020-08-04 10:51:20 -0700

    creating event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,13 @@
 class Event < ApplicationRecord
+    before_validation :set_default_price
+
+    validates :title, presence: true, uniqueness: true
+    validates :description, presence: true, length: { minimum: 100 }
+    validates :location, presence: true
+    validates :price, presence: true
+    validates :image, presence: true
+    private
+    def set_default_price
+        self.price ||=0
+    end
 end
