6313bf629cef614d0f6e46945b68b35be3888e38
Harsimranjit Kaur <harsimranchahal170@gmail.com>
2020-08-04 17:05:32 -0700

    adding bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_225739) do
+ActiveRecord::Schema.define(version: 2020_08_04_231310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 2020_08_04_225739) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "participants"
     t.datetime "start_time"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
